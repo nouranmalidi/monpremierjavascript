58773bfd9ce7587c5d6370074df9aac6bf660a46
nouranmalidi <nouranmalidi@gmail.com>
2025-12-04 16:02:19 +0300

    Utilisation de javaScript avec le jeu de gestion de bibliotheque

---
+++ gestionBibliotheque.js
@@ -0,0 +1,20 @@
+// Ici je fait mon jeu
+
+let totalLivres = 500; // Création des tous les variables nécéssaire pour le jeu
+console.log(totalLivres);
+
+let acheter = 50;
+console.log(acheter);
+
+let jeter = 10;
+console.log(jeter);
+
+let racheter = 5;
+console.log(racheter);
+
+console.log(totalLivres + acheter - jeter + racheter); // Vérification du résultat grâce a console.log
+
+
+let affichageTotalLivres = console.log(totalLivres + acheter - jeter + racheter); /* Création d'une nouvelle variable 
+affichageTotalLivres et utilisation de consolole.log  pour affichez la phrase*/
+
---
+++ script.js
@@ -0,0 +1,58 @@
+        // Ici, je rédige mon script JavaScript
+        let nom = "BACAR SAID";  /*--variable de type string, chaines de caractères--*/  //Je déclare la variable nom qui est est 
+        // relié à BACAR SAID-->
+        let prenom = "Abdou";  //Je déclare la variable prenom qui  est relié a Nouran-->
+        let prixNetflix= 19.99;   /*--varaible de type number, nombre*/
+        let age = 23;
+       
+        let salutation = "Bonjour les gens";
+        let greeting = 'Good morning';
+        let andabu = 'Kwezi';
+        let commune = "M'tsamboro";
+      
+        /*--L'antislash permet de de pas prendre en compte l'apostrophe*/
+        let village = 'M\'tsamboro';
+       
+        let vrai = true; /*--variable de type boolean permettent de mettre des des information qui sont soit vraie ou fausse.*/
+        let est_faux = false;
+       
+        /*Variable de type Array permet de faire un tableau pour stocker une liste d'information.*/
+        let eleves = ["Nouran", "Azilati","Chadia", "Mchagama", "Bonobo", "Oustad"];
+        
+        /*Variable de type objets qui peut contenire plusieurs propriétés qui on leurs propre valeur.*/
+        let client = {
+            nom: "Bacar",
+            village: "Choungui",
+            age: 25,
+            scores: [4,7,2]
+
+        };
+
+        const prixTomate = 3.5;
+        const nombreFraction = 1/3;
+        const nombreNegatif = -509;
+        const nombreDecimal = 3.4123;
+        
+        console.log("Bonjour,je m'appelle",prenom,nom,"j'ai",age,"j'ai payé l'abonnement à",prixNetflix);
+        console.log("J'ai payé ce kilo de tomate à",prixTomate);
+        console.log("Mon compte en banque affiche actuellement",nombreNegatif);
+        console.log("En Asie les pizza sont découpé en",nombreFraction);
+        
+        console.log(salutation);
+        console.log(greeting);
+        console.log(andabu);
+       
+        console.log("j'habite à",commune);
+        console.log("j'habite à",village);
+        console.log("Je suis au RSMA",vrai);
+        
+        console.log(eleves); /*Affichage du tableau eleves*/
+        console.log(eleves[0]); /*Affichage des membres du tableau eleves avec les indinces*/
+        console.log(eleves[1]);
+        console.log(eleves[2]); 
+        
+        console.log(client.nom); /*Affichage de l'objet client avec la valeur nom*/
+        console.log(client.village);
+        console.log(client.age);
+        console.log(client.scores);
+        console.log(client.scores[1]); /*Affichage du tableau scores avec l'indice 1 qui se trouve dans l'objet client */
